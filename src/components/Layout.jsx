4ebee724e085e2bafa45fb99ed9fd84f53b6abde
covid2020-w1 <dwjelke@gmail.com>
2026-08-31 16:03:53 -0500

    added some styling and content to home

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,19 @@ import initials from '../assets/initials.webp'
 import { Outlet, NavLink } from 'react-router-dom'
 
 export default function Layout(){
+    const style = {
+        color: 'red'
+    }
+
     return(
         <>
-            <header>
+            <header className='flex bg-blue-100 justify-between p-4 items-center mb-4'>
                 <NavLink to=".">
-                    <img src={initials} width="50px"/>
+                    <img src={initials} className='w-12'/>
                 </NavLink>
-                <nav>
-                    <NavLink to="about">About</NavLink>
-                    <NavLink to="philosophy">Philosophy</NavLink>
+                <nav className='flex'>
+                    <NavLink to="about" style={ ({isActive}) => isActive ? style : null} className='pl-4'>About</NavLink>
+                    <NavLink to="philosophy" style={ ({isActive}) => isActive ? style : null}  className='pl-4'>Philosophy</NavLink>
                 </nav>
             </header>
             <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,57 @@
 import memoji from '../assets/memoji.webp'
 import signature from '../assets/signature.gif'
+import nmVideo from '../assets/nm-trailer.mp4'
+import budderTrailerOne from '../assets/budder-trailer-1.mp4'
+import budderTrailerTwo from '../assets/budder-trailer-2.mp4'
+import mclResearchPreview from '../assets/mcl-research-preview.webp'
+import mclDsPreview from '../assets/mcl-ds-preview.webp'
+import budderPreview from '../assets/budder-preview.webp'
+
 
 export default function Home(){
     return(
         <main>
-            <h1>Hi, my name is David</h1>
-            <img src={memoji} />
-            <h2>I'm a UX Designer and Researcher</h2>
-            <h3>And I design to help people lead long, healthy. lives and have fun along the way</h3>
-            <img src={signature} width="400px"/>
+            <section className='px-8'>
+                <div className="flex items-center gap-4 mb-6">
+                    <h1 className='text-2xl order-1 text-right'>Hi, my name is <span className='text-blue-500'>David</span>.</h1>
+                    <img src={memoji} className='w-24'/>
+                </div>
+                <h1 className='text-2xl mb-6'>I'm a <span className='text-blue-500'>UX Designer</span> and <span className='text-blue-500'>Researcher</span>.</h1>
+                <h1 className='text-xl text-center'>And I design to help people lead <span className='text-blue-500'>healthy </span>lives and have <span className='text-blue-500'>fun</span> along the way.</h1>
+                <img src={signature} className='w-48 mx-auto my-6'/>
+                <h3 className='text-xl text-center mb-6'>...also, I have 3 superpowers</h3>
+                <h2 className='text-6xl text-center mb-6'>🦸🏻‍♂️</h2>
+            </section>
+            <section className='px-8 bg-blue-50 pt-6 pb-6'>
+                <ol className='list-decimal flex flex-col gap-6'>
+                    <li>
+                        <p className='mb-2'>I spot unsung opportunities</p>
+                        <video src={budderTrailerTwo} />
+                    </li>
+                    <li>
+                        <p className='mb-2'>I wrangle unruly data</p>
+                        <video src={nmVideo} />
+                    </li>
+                    <li>
+                        <p className='mb-2'>I tell human-centered stories</p>
+                        <video src={budderTrailerOne} />
+                    </li>
+                </ol>
+            </section>
+            <section className='px-8 pt-6 pb-6'>
+                <h2 className='text-2xl py-6 text-center'>Projects</h2>
+                <ul className='flex flex-col gap-6'>
+                    <li>
+                        <img src={mclResearchPreview} className='w-80 mx-auto'/>
+                    </li>
+                    <li>
+                        <img src={mclDsPreview} className='w-80 mx-auto'/>
+                    </li>
+                    <li>
+                        <img src={budderPreview} className='w-80 mx-auto'/>
+                    </li>
+                </ul>
+            </section>
         </main>
     )
 }

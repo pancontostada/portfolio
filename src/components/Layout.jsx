bb31417a29b92801d4f2b4e5a08af7e9902fb58e
covid2020-w1 <dwjelke@gmail.com>
2026-09-05 23:46:04 -0500

    deleted distracting comments

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,35 +11,35 @@ export default function Layout(){
         fontWeight: 'bold'
     }
 
-    function handleScroll(event){ //when the div scroll container containing the outlet scrolls...
-        const scrollTop = event.currentTarget.scrollTop //save a value that measures how far the top of the Outlet element is from the top of the containing div element
-        const scrollDifference = scrollTop - lastScrollTop.current //determines the direction of the scroll. if the value is positive, then the user is scrolling down.
+    function handleScroll(event){ 
+        const scrollTop = event.currentTarget.scrollTop 
+        const scrollDifference = scrollTop - lastScrollTop.current 
 
         if (scrollTop <= 0) {
-            setIsHeaderVisible(true) //if the user hasn't scrolled, then set visibility to true
-        } else if (Math.abs(scrollDifference) >= 4) { //i think this is to measure the speed the user is scrolling? maybe if the user scrolls fast enough, the next time the browsers able to update the value of lastScrollTop will be greater than 4, thus kicking in the condition 
-            setIsHeaderVisible(scrollDifference < 0) //if user scrolls down, scrollTop will be greater than lastScrollTop, then expression evaluates to false, the header is set to false! this triggers the height of the div class to equal 0, and triggers the header to move up out of view. but why do both actions have to happen? why cant the div height being set to 0 be enough?
+            setIsHeaderVisible(true)
+        } else if (Math.abs(scrollDifference) >= 4) {
+            setIsHeaderVisible(scrollDifference < 0)
         }
 
-        lastScrollTop.current = scrollTop //update value of lastScrollTop
+        lastScrollTop.current = scrollTop 
     }
 
-    useLayoutEffect(() => { //run on first render, before browser has painted
+    useLayoutEffect(() => { 
         const updateHeaderHeight = () => {
-            setHeaderHeight(headerRef.current?.offsetHeight ?? 0) //set the headerHeight to whatever the natural value is based on its content. if for some reason the ref is still null (maybe cause header gets removed?), then set the height to 0
+            setHeaderHeight(headerRef.current?.offsetHeight ?? 0)
         }
 
         updateHeaderHeight() //run the fn
-        const resizeObserver = new ResizeObserver(updateHeaderHeight) //create in essence an event listener that listens for change in the size. what im confused by, is wouldnt this run any time the dimensions of any element on the page gets resized??
+        const resizeObserver = new ResizeObserver(updateHeaderHeight)
         resizeObserver.observe(headerRef.current)
 
-        return () => resizeObserver.disconnect() //remove the event listener, but not sure when 
+        return () => resizeObserver.disconnect()
     }, [])
 
     return(
-        <div className='h-dvh flex flex-col'> {/* let height of div be whole page, accouting for address bar */}
-            <div className='shrink-0 overflow-hidden transition-[height] duration-300' style={{ height: isHeaderVisible ? headerHeight : 0 }}> {/* don't let the header shrink vertically. */}
-                <header ref={headerRef} className={`flex bg-blue-100 justify-between p-4 items-center lg:p-8 transition-transform duration-300 ${isHeaderVisible ? 'translate-y-0' : '-translate-y-full'}`}> {/* if state is false, shift the header up out of view */}
+        <div className='h-dvh flex flex-col'>
+            <div className='shrink-0 overflow-hidden transition-[height] duration-300' style={{ height: isHeaderVisible ? headerHeight : 0 }}> 
+                <header ref={headerRef} className={`flex bg-blue-100 justify-between p-4 items-center lg:p-8 transition-transform duration-300 ${isHeaderVisible ? 'translate-y-0' : '-translate-y-full'}`}> 
                     <NavLink to=".">
                         <img src={initials} className='w-12'/>
                     </NavLink>
@@ -50,7 +50,7 @@ export default function Layout(){
                     </nav>
                 </header>
             </div>
-            <div className='min-h-0 flex-1 overflow-y-auto' onScroll={handleScroll}> {/* allow the scroll container to shrink to fill up remaining page, establish a vertical scroll context, add the onScroll evt listener, then in outlet make sure the child element has h-full*/}
+            <div className='min-h-0 flex-1 overflow-y-auto' onScroll={handleScroll}> 
                 <Outlet />
             </div>
         </div>
